ba5a584bd9b7446bef2af3cd59a28bc67b5c4a60
Martín Stancanelli <mastanca14@gmail.com>
2020-10-23 15:38:21 -0300

    rename relational db file

--- domain/ticket/sqlite_repository.go
+++ domain/ticket/sqlite_repository.go
@@ -11,11 +11,11 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type mysqlRepository struct {
+type sqliteRepository struct {
 	database *sqlx.DB
 }
 
-func NewMysqlRepository() (*mysqlRepository, error) {
+func NewMysqlRepository() (*sqliteRepository, error) {
 	var err error
 	database, err := sqlx.Connect("sqlite3", "challenge_db")
 	if err != nil {
@@ -25,17 +25,17 @@ func NewMysqlRepository() (*mysqlRepository, error) {
 	// TODO: This setup could be done directly into the db or using a migration lib
 	database.MustExec(schema)
 
-	return &mysqlRepository{database: database}, nil
+	return &sqliteRepository{database: database}, nil
 }
 
-func (m mysqlRepository) Save(ctx context.Context, ticket Ticket) error {
+func (m sqliteRepository) Save(ctx context.Context, ticket Ticket) error {
 	if _, err := m.database.Exec("insert into tickets (id, country, event) values ($1, $2, $3)", uuid.New().String(), ticket.Country, ticket.Event); err != nil {
 		return errors.Wrap(err, "an error occurred while saving a ticket sale")
 	}
 	return nil
 }
 
-func (m mysqlRepository) GetAll(ctx context.Context) (Tickets, error) {
+func (m sqliteRepository) GetAll(ctx context.Context) (Tickets, error) {
 	rows, err := m.database.Queryx("SELECT * FROM tickets")
 	if err != nil {
 		return nil, errors.Wrap(err, "error fetching all tickets")
@@ -52,7 +52,7 @@ func (m mysqlRepository) GetAll(ctx context.Context) (Tickets, error) {
 	return result, nil
 }
 
-var _ Repository = (*mysqlRepository)(nil)
+var _ Repository = (*sqliteRepository)(nil)
 
 var schema = `		
 	create table if not exists tickets
